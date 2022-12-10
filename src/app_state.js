28c6319ff594c6b427288bf3cbe3b2e2ce37576f
Alex Garcia <hi.im.alex.j.garcia@gmail.com>
2022-12-09 22:53:10 -0700

    Using pubsub to update UI components instead of hashchange, updating hash via replaceState, cleaned up js.

--- src/app_state.js
+++ src/app_state.js
@@ -1,34 +1,38 @@
-export default function AppState() {
+import PubSub from 'pubsub-js'
 
+export default function AppState () {
   const state = new Proxy({
     count: 0,
-    notes: [],
+    notes: []
   }, {
     set: (obj, prop, value) => {
-      obj[prop] = value;
-      window.location.hash = '#/' + btoa(JSON.stringify(state));
-      return true;
+      obj[prop] = value
+      window.history.replaceState(null, null, document.location.pathname + '#/' + btoa(JSON.stringify(obj)))
+      PubSub.publish('updateState', { prop, obj })
+      return true
     }
-  });
+  })
 
   return {
     state: {
-      getEncoded: () => btoa(JSON.stringify(state)),
-      setEncoded: s => Object.assign(state, JSON.parse(atob(s))),
+      setEncoded: s => Object.assign(state, JSON.parse(atob(s)))
     },
     counter: {
-      increment: () => state.count += 1,
-      decrement: () => state.count -= 1,
+      increment: () => { state.count += 1 },
+      decrement: () => { state.count -= 1 },
       get: () => state.count
     },
     notes: {
       getAll: () => [...state.notes],
       add: n => {
-        state.notes = [...state.notes, n];
+        state.notes = [...state.notes, n]
       },
       remove: n => {
-        state.notes = state.notes.filter((node) => node !== n);
+        state.notes = state.notes.filter((node) => node !== n)
       }
+    },
+    pubsub: {
+      subscribe: (f) => { return PubSub.subscribe('updateState', f) }
     }
-  };
+  }
 }
--- src/index.js
+++ src/index.js
@@ -1,79 +1,87 @@
-import { v4 as uuidv4 } from 'uuid';
-import { LoremIpsum } from 'lorem-ipsum';
+/* eslint-env browser */
 
-import './index.css';
-import AppState from './app_state';
+import { v4 as uuidv4 } from 'uuid'
+import { LoremIpsum } from 'lorem-ipsum'
 
-const App = AppState();
+import './index.css'
+import AppState from './app_state'
+
+const App = AppState()
 
 // Set up counter
-const counterDisplay = document.getElementById('counterDisplay');
-const counterButtonDecrement = document.getElementById('counterButtonDecrement');
-const counterButtonIncrement = document.getElementById('counterButtonIncrement');
+const counterDisplay = document.getElementById('counterDisplay')
+const counterButtonDecrement = document.getElementById('counterButtonDecrement')
+const counterButtonIncrement = document.getElementById('counterButtonIncrement')
 
-counterButtonIncrement.addEventListener('click', () => App.counter.increment(), false);
-counterButtonDecrement.addEventListener('click', () => App.counter.decrement(), false);
+counterButtonIncrement.addEventListener('click', () => App.counter.increment(), false)
+counterButtonDecrement.addEventListener('click', () => App.counter.decrement(), false)
 
-window.addEventListener('hashchange', () => {
-  counterDisplay.innerHTML = App.counter.get();
-}, false);
-
+App.pubsub.subscribe((msg, data) => {
+  if (data.prop === 'count') {
+    counterDisplay.innerHTML = data.obj.count
+  }
+})
 
 // Set up notes
-const ipsum = new LoremIpsum();
-const notesContainer = document.getElementById('notesContainer');
-const notesButtonAdd = document.getElementById('notesButtonAdd');
-const notesDisplayCount = document.getElementById('notesDisplayCount');
-const noteTemplate = document.getElementById('noteTemplate').content;
+const ipsum = new LoremIpsum()
+const notesContainer = document.getElementById('notesContainer')
+const notesButtonAdd = document.getElementById('notesButtonAdd')
+const notesDisplayCount = document.getElementById('notesDisplayCount')
+const noteTemplate = document.getElementById('noteTemplate').content
 
 notesButtonAdd.addEventListener('click', () => {
   App.notes.add({
     id: uuidv4(),
     title: ipsum.generateSentences(1),
     body: Math.floor(Math.random() * 2) ? ipsum.generateParagraphs(1) : undefined
-  });
-}, false);
+  })
+}, false)
 
-window.addEventListener('hashchange', () => {
-  const notesEl = document.createElement('div');
+App.pubsub.subscribe((msg, data) => {
+  if (data.prop === 'notes') {
+    const notesEl = document.createElement('div')
 
-  App.notes.getAll().forEach((note) => {
-    const el = noteTemplate.firstElementChild.cloneNode(true);
-    const button = el.querySelector('button');
-    const cardTitle = el.querySelector('.card-title');
-    const cardText = el.querySelector('.card-text');
+    data.obj.notes.forEach((note) => {
+      const el = noteTemplate.firstElementChild.cloneNode(true)
+      const button = el.querySelector('button')
+      const cardTitle = el.querySelector('.card-title')
+      const cardText = el.querySelector('.card-text')
 
-    el.dataset.id = note.id;
-    cardTitle.innerHTML = note.title;
-    note.body ? cardText.innerHTML = note.body : null;
+      el.dataset.id = note.id
+      cardTitle.innerHTML = note.title
+      if (note.body) cardText.innerHTML = note.body
 
-    button.addEventListener('click', (e) => {
-      const id = e.target.parentNode.parentNode.parentNode.dataset.id;
-      const note = App.notes.getAll().find((note) => note.id === id);
-      App.notes.remove(note);
-    });
+      button.addEventListener('click', (e) => {
+        const id = e.target.parentNode.parentNode.parentNode.dataset.id
+        const note = data.obj.notes.find((note) => note.id === id)
+        App.notes.remove(note)
+      })
 
-    notesEl.appendChild(el);
-  });
+      notesEl.appendChild(el)
+    })
 
-  notesContainer.replaceChildren(notesEl);
+    notesContainer.replaceChildren(notesEl)
 
-  notesDisplayCount.innerHTML = `${App.notes.getAll().length} notes`;
-}, false);
-
+    notesDisplayCount.innerHTML = `${App.notes.getAll().length} notes`
+  }
+})
 
 // Set up Copy URL button
-const buttonCopyURL = document.getElementById('buttonCopyURL');
+const buttonCopyURL = document.getElementById('buttonCopyURL')
 
 buttonCopyURL.addEventListener('click', () => {
-  navigator.clipboard.writeText(location.href);
-}, false);
-
+  navigator.clipboard.writeText(location.href)
+}, false)
 
 // Initalize App state
-try {
-  App.state.setEncoded(window.location.hash.substring(2));
-} catch (e) {
-  console.error(e);
-  App.state.setEncoded('eyJjb3VudCI6MCwibm90ZXMiOltdfQ=='); // 'eyJjb3VudCI6MCwibm90ZXMiOltdfQ==' base64 for { count: 0, notes: [] }
-}
+window.addEventListener('hashchange', () => {
+  try {
+    App.state.setEncoded(window.location.hash.substring(2))
+  } catch (e) {
+    console.error(e)
+    App.state.setEncoded('eyJjb3VudCI6MCwibm90ZXMiOltdfQ==') // 'eyJjb3VudCI6MCwibm90ZXMiOltdfQ==' base64 for>
+  }
+}, false)
+
+// Initial render
+window.dispatchEvent(new HashChangeEvent('hashchange'))
